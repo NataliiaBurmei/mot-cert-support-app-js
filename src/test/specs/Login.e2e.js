671a6b825d4fa3174e0e900ae47174a3d80a8b55
NataliiaBurmei <nataliyaburmey@gmail.com>
2024-07-02 15:57:03 +0100

    Testing some hypothesis

--- src/test/specs/Login.e2e.js
+++ src/test/specs/Login.e2e.js
@@ -13,4 +13,15 @@ describe('My login application', () => {
         await expect (element).toHaveText('Projects')
 
     })
+
+    it('should not login with invalid credentials', async () => {
+        await browser.url('http://localhost:3000/#/login')
+
+        await $('input[name=email]').setValue('invalidemail.com')
+        await $('input[name=password]').setValue('invalidpassword')
+        await $('button').click()
+        
+        const element = await $('.card-title')
+        await expect (element).toHaveText('Projects')
+    })
 })
